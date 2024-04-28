e39962659c59e74048194f021858929bb7919146
Gabriel Felipe <129997625+gabszinn@users.noreply.github.com>
2024-04-27 21:16:31 -0300

    Add files via upload

---
+++ LAV2/OrganizadorMochila.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class OrganizadorMochila {
+
+  public static int max(int a, int b) {
+    return (a > b)? a : b;
+  }
+
+  public static int organizarMochila(int capacidade, int pesoItens[], int valItens[], int n) {
+    if (n == 0 || Arrays.stream(pesoItens).sum() == 0 )
+      return 0;
+    if (pesoItens[n-1] > capacidade)
+      return organizarMochila(capacidade, pesoItens, valItens, n - 1);
+    else
+      return max(valItens[n-1] + organizarMochila(capacidade - pesoItens[n-1], pesoItens, valItens, n-1), organizarMochila(capacidade, pesoItens, valItens, n-1));
+  }
+
+}
